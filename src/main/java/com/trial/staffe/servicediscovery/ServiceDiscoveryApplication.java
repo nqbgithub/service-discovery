0d64a1b2a57d1d25fee0ffc576f554dcb3f64ed0
nqb13510951690 <nqb13510951690@sina.com>
2018-04-27 15:49:01 +0800

    update

---
+++ src/main/java/com/trial/staffe/servicediscovery/ServiceDiscoveryApplication.java
@@ -0,0 +1,16 @@
+package com.trial.staffe.servicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 服务注册中心
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceDiscoveryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDiscoveryApplication.class, args);
+    }
+}
